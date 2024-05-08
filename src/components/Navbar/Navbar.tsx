4a29e865cfe04752d5620920bf9cedd3f6d87910
luanborher <luan.borher@gmail.com>
2024-05-08 10:54:16 -0300

    fix: navbar adjustment

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -80,7 +80,7 @@ const Navbar = () => {
           <NavLink href="/users" selected={pathname.startsWith('/users')}>
             <UserIcon />
             <NavLinkText selected={pathname.startsWith('/users')}>
-              Usuários construtora
+              Cadastro de usuário construtora
             </NavLinkText>
           </NavLink>
         )}
