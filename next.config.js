3afa6124048a91e4c820b5143e2f1d8502f6bcf6
luanborher <luan.borher@gmail.com>
2024-10-21 16:37:32 -0300

    fix: next config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ module.exports = {
       ssr: true,
     },
   },
+  images: {
+    domains: ['api.manualpredial.com.br'],
+  },
   webpack: config => {
     config.module.rules.push({
       test: /\.svg$/,
